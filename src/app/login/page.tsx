7ee0b019a23edce3cbfc1f999c2bd4558de70cc4
c-goodman <cbgoodm@gmail.com>
2025-01-03 17:29:17 -0500

    Link to index on logo in login page instead, minor css changes to make header more similar

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,20 @@
-import Logo from '@/app/ui/mk-logo';
-import LoginForm from '@/app/ui/login-form';
+import Logo from "@/app/ui/mk-logo";
+import LoginForm from "@/app/ui/login-form";
+import Link from "next/link";
 
 export default function LoginPage() {
-    return (
-        <main className="flex items-center justify-center md:h-screen">
-            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <div className="flex h-20 w-full items-end rounded-lg bg-red-500 p-3 md:h-36">
-                    <div className="w-32 text-white md:w-36">
-                        <Logo />
-                    </div>
-                </div>
-                <LoginForm />
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-600 p-4 md:h-40">
+          <div className="text-white">
+            <Link href="/">
+              <Logo />
+            </Link>
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
