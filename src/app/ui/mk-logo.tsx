bd01f98cd56b65f8206d5d303f58cb77f192cb6f
c-goodman <cbgoodm@gmail.com>
2025-01-03 17:18:35 -0500

    Add homepage link to logo

--- src/app/ui/mk-logo.tsx
+++ src/app/ui/mk-logo.tsx
@@ -1,13 +1,16 @@
-import { FlagIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { FlagIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+import Link from "next/link";
 
 export default function MKLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <FlagIcon className="h-11 w-11 rotate-[-10deg]" />
-      <p className="flex text-[44px]">MK Data</p>
-    </div>
+    <Link href="/">
+      <div
+        className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      >
+        <FlagIcon className="h-11 w-11 rotate-[-10deg]" />
+        <p className="flex text-[44px]">MK Data</p>
+      </div>
+    </Link>
   );
 }
