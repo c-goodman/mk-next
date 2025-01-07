dddb95bd087e4e640bd8acb96b1922c7ebad6c88
c-goodman <cbgoodm@gmail.com>
2025-01-06 19:25:01 -0500

    Unify logo and painted container for mobile layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,14 @@
-import Logo from "@/app/ui/mk-logo";
+import MKLogo from "@/app/ui/mk-logo";
 import LoginForm from "@/app/ui/login-form";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-600 p-4 md:h-40">
-          <div className="text-white">
-            <Link href="/">
-              <Logo />
-            </Link>
-          </div>
-        </div>
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-6 md:-mt-32">
+        <Link href="/">
+          <MKLogo />
+        </Link>
         <LoginForm />
       </div>
     </main>
--- src/app/ui/mk-logo.tsx
+++ src/app/ui/mk-logo.tsx
@@ -2,11 +2,13 @@ import { FlagIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 export default function MKLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <FlagIcon className="h-11 w-11 rotate-[-10deg]" />
-      <p className="flex text-[44px]">MK Data</p>
+    <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-600 p-4 md:h-40">
+      <div
+        className={`${lusitana.className} flex flex-row items-start leading-none text-white`}
+      >
+        <FlagIcon className="h-8 w-8 rotate-[-10deg]" />
+        <p className="flex text-[38px]">MK Data</p>
+      </div>
     </div>
   );
 }
