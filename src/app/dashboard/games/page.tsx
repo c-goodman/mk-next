c9e244dfe4a9e5065f68bae5103fd2ccff2f07b3
c-goodman <cbgoodm@gmail.com>
2025-01-28 18:24:56 -0500

    Games page

---
+++ src/app/dashboard/games/page.tsx
@@ -0,0 +1,42 @@
+import { fetchGamesPages } from "@/app/lib/data";
+import { lusitana } from "@/components/ui/fonts";
+import LatestGamesTable from "@/components/ui/games/latest-games-table";
+import { CreateInvoice } from "@/components/ui/invoices/buttons";
+import Pagination from "@/components/ui/invoices/pagination";
+
+import Search from "@/components/ui/search";
+import { InvoicesTableSkeleton } from "@/components/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page(props: {
+  // Define optional search params that can be passed to the route URL
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchGamesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Games</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search games..." />
+        {/* <CreateInvoice /> */}
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}> */}
+      <Suspense key={query + currentPage}>
+        <LatestGamesTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
