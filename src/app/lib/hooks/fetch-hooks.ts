a69ad32ad9840bef3f1ffdcc0e3df35328c6d286
c-goodman <cbgoodm@gmail.com>
2025-02-06 20:49:58 -0500

    fetch hook for maps and characters

--- src/app/lib/hooks/fetch-hooks.ts
+++ src/app/lib/hooks/fetch-hooks.ts
@@ -1,6 +1,14 @@
 "use server";
 
-import { fetchUserByEmail, fetchUserByName, fetchUserNames } from "../data";
+// import { cache } from "react";
+import {
+  fetchCharacters,
+  fetchMaps,
+  fetchUserByEmail,
+  fetchUserByName,
+  fetchUserNames,
+} from "../data";
+import { TCharactersTable, TMapsTable } from "../definitions";
 
 export async function useFetchUserNames(): Promise<string[]> {
   const [userNames] = await Promise.all([fetchUserNames()]);
@@ -19,3 +27,22 @@ export async function useFetchUserByEmail(email: string) {
 
   return user;
 }
+
+export async function useFetchCharacters(): Promise<TCharactersTable[]> {
+  const [characters] = await Promise.all([fetchCharacters()]);
+
+  return characters;
+}
+
+export async function useFetchMaps(): Promise<TMapsTable[]> {
+  const [maps] = await Promise.all([fetchMaps()]);
+
+  return maps;
+}
+
+// export const useFetchCharacters = cache(async () => {
+//   const characters = await fetchCharacters();
+
+//   if (!characters) notFound();
+//   return characters;
+// });
