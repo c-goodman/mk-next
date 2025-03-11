144cb4eecde6dae87d1b08fea01726ce22b7cd30
c-goodman <cbgoodm@gmail.com>
2025-03-11 17:52:09 -0400

    Fetch and display total rolling wins for first place player in latest games table

--- src/components/ui/dashboard/latest-games.tsx
+++ src/components/ui/dashboard/latest-games.tsx
@@ -1,13 +1,28 @@
 import clsx from "clsx";
 import Image from "next/image";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
-import { fetchCharacters, fetchLatestGames } from "@/app/lib/data";
+import {
+  fetchCharacters,
+  fetchLatestGames,
+  fetchSessionPlacementsRollingAllGameTypesFirst,
+  fetchSessionPlacementsRollingFourPlayerFirst,
+  fetchSessionPlacementsRollingThreePlayerFirst,
+  fetchSessionPlacementsRollingTwoPlayerFirst,
+} from "@/app/lib/data";
 import { lusitana } from "@/components/ui/fonts";
 import { TGamesTable } from "@/app/lib/definitions";
 
 export default async function LatestGames() {
   const latestGames = await fetchLatestGames();
   const characters = await fetchCharacters();
+  const sessionPlacementsAllGamesFirstPlace =
+    await fetchSessionPlacementsRollingAllGameTypesFirst();
+  const sessionPlacementsFourPlayerFirstPlace =
+    await fetchSessionPlacementsRollingFourPlayerFirst();
+  const sessionPlacementsThreePlayerFirstPlace =
+    await fetchSessionPlacementsRollingThreePlayerFirst();
+  const sessionPlacementsTwoPlayerFirstPlace =
+    await fetchSessionPlacementsRollingTwoPlayerFirst();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -17,6 +32,44 @@ export default async function LatestGames() {
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-2">
           {latestGames.map((data: TGamesTable, i) => {
+            // TODO: thanks Mo :)
+            // TODO: Use .filter() on the table arrays?
+
+            const firstPlaceTotalSessionWins =
+              sessionPlacementsAllGamesFirstPlace?.find(
+                (g) =>
+                  g.suid === data.suid &&
+                  g.player_name === data.players_1st &&
+                  g.id === data.id
+              )?.running_total;
+
+            const firstPlaceTotalSessionWinsTwoPlayer =
+              sessionPlacementsTwoPlayerFirstPlace?.find(
+                (g) =>
+                  g.suid === data.suid &&
+                  data.players === 2 &&
+                  g.player_name === data.players_1st &&
+                  g.id <= data.id
+              )?.running_total;
+
+            const firstPlaceTotalSessionWinsThreePlayer =
+              sessionPlacementsThreePlayerFirstPlace?.find(
+                (g) =>
+                  g.suid === data.suid &&
+                  data.players === 3 &&
+                  g.player_name === data.players_1st &&
+                  g.id === data.id
+              )?.running_total;
+
+            const firstPlaceTotalSessionWinsFourPlayer =
+              sessionPlacementsFourPlayerFirstPlace?.find(
+                (g) =>
+                  g.suid === data.suid &&
+                  data.players === 4 &&
+                  g.player_name === data.players_1st &&
+                  g.id === data.id
+              )?.running_total;
+
             return (
               <div
                 key={data.id}
@@ -128,6 +181,24 @@ export default async function LatestGames() {
                         {data.characters_4th}
                       </div>
                     )}
+                  </div>
+                  <div className="hidden min-w-0 mr-4 sm:block">
+                    <div className="items-end truncate text-sm md:text-base">
+                      {firstPlaceTotalSessionWins &&
+                        `Total Wins: ${firstPlaceTotalSessionWins}`}
+                    </div>
+                    <div className="truncate text-sm md:text-base">
+                      {firstPlaceTotalSessionWinsFourPlayer &&
+                        `4P Wins: ${firstPlaceTotalSessionWinsFourPlayer}`}
+                    </div>
+                    <div className="truncate text-sm md:text-base">
+                      {firstPlaceTotalSessionWinsThreePlayer &&
+                        `3P Wins: ${firstPlaceTotalSessionWinsThreePlayer}`}
+                    </div>
+                    <div className="truncate text-sm md:text-base">
+                      {firstPlaceTotalSessionWinsTwoPlayer &&
+                        `2P Wins: ${firstPlaceTotalSessionWinsTwoPlayer}`}
+                    </div>
                   </div>
                 </div>
               </div>
