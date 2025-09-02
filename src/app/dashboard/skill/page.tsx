65eb71260d3420e723b13cb0bb22a236cf0d499e
c-goodman <cbgoodm@gmail.com>
2025-09-01 21:48:41 -0400

    Remove unused suspense boundary

--- src/app/dashboard/skill/page.tsx
+++ src/app/dashboard/skill/page.tsx
@@ -1,12 +1,9 @@
 import SeasonalSkillChart from "@/components/ui/skill/seasonal-skill-chart";
-import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <div className="w-full">
-      <Suspense>
-        <SeasonalSkillChart />
-      </Suspense>
+      <SeasonalSkillChart />
     </div>
   );
 }
